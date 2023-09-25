bbc57ff97072e8b896ca2011a5236b3f2edaf9d3
paulps <paul_ch_wu@cloud-latitude.com>
2023-09-25 22:26:08 +0800

    update

--- test/scheduler/scheduler_3_test.dart
+++ test/scheduler/scheduler_3_test.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 
-import 'package:fake_async/fake_async.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
--- test/scheduler/scheduler_4_test.dart
+++ test/scheduler/scheduler_4_test.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
